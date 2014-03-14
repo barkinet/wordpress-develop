07fc2dc4b6b74b27edd93f0e6502574dfc784d67
Andrea Fercia <afercia@git.wordpress.org>
2014-03-14 10:26:15 -0700

    27417: recent posts widget default number of posts to show

--- src/wp-includes/default-widgets.php
+++ src/wp-includes/default-widgets.php
@@ -561,9 +561,9 @@ class WP_Widget_Recent_Posts extends WP_Widget {
 
 		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Posts' );
 		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
-		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 10;
+		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
 		if ( ! $number )
- 			$number = 10;
+ 			$number = 5;
 		$show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
 
 		$r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ) ) );
