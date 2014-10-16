f669141e3a0de1787122db674b855e3d1ce251b6
Weston Ruter <weston@xwp.co>
2014-10-15 22:11:01 -0700

    Access Customizer controls from the API in the proper way
    Don't manually update control state since setting change will do this automatically now.

--- src/wp-content/themes/twentyfifteen/js/color-scheme-control.js
+++ src/wp-content/themes/twentyfifteen/js/color-scheme-control.js
@@ -5,46 +5,37 @@
  * Adds listener to Color Scheme control to update other color controls with new values/defaults
  */
 
-( function( wp ) {
-	wp.customize.controlConstructor.colorScheme = wp.customize.Control.extend( {
+( function( customize ) {
+	customize.controlConstructor.colorScheme = customize.Control.extend( {
 		ready: function() {
-			var parentSection    = this.container.closest( '.control-section' ),
-				headerTextColor  = parentSection.find( '#customize-control-header_textcolor .color-picker-hex' ),
-				backgroundColor  = parentSection.find( '#customize-control-background_color .color-picker-hex' ),
-				sidebarColor     = parentSection.find( '#customize-control-header_background_color .color-picker-hex' ),
-				sidebarTextColor = parentSection.find( '#customize-control-sidebar_textcolor .color-picker-hex' );
 
 			this.setting.bind( 'change', function( value ) {
 				// if Header Text is not hidden, update value
-				if ( 'blank' !== wp.customize( 'header_textcolor' ).get() ) {
-					wp.customize( 'header_textcolor' ).set( colorScheme[value].colors[4] );
-					headerTextColor.val( colorScheme[value].colors[4] )
+				if ( 'blank' !== customize( 'header_textcolor' ).get() ) {
+					customize( 'header_textcolor' ).set( colorScheme[value].colors[4] );
+					customize.control( 'header_textcolor' ).container.find( '.color-picker-hex' )
 						.data( 'data-default-color', colorScheme[value].colors[4] )
-						.wpColorPicker( 'color', colorScheme[value].colors[4] )
 						.wpColorPicker( 'defaultColor', colorScheme[value].colors[4] );
 				}
 
 				// update Background Color
-				wp.customize( 'background_color' ).set( colorScheme[value].colors[0] );
-				backgroundColor.val( colorScheme[value].colors[0] )
+				customize( 'background_color' ).set( colorScheme[value].colors[0] );
+				customize.control( 'background_color' ).container.find( '.color-picker-hex' )
 					.data( 'data-default-color', colorScheme[value].colors[0] )
-					.wpColorPicker( 'color', colorScheme[value].colors[0] )
 					.wpColorPicker( 'defaultColor', colorScheme[value].colors[0] );
 
 				// update Header/Sidebar Background Color
-				wp.customize( 'header_background_color' ).set( colorScheme[value].colors[1] );
-				sidebarColor.val( colorScheme[value].colors[1] )
+				customize( 'header_background_color' ).set( colorScheme[value].colors[1] );
+				customize.control( 'header_background_color' ).container.find( '.color-picker-hex' )
 					.data( 'data-default-color', colorScheme[value].colors[1] )
-					.wpColorPicker( 'color', colorScheme[value].colors[1] )
 					.wpColorPicker( 'defaultColor', colorScheme[value].colors[1] );
 
 				// update Sidebar Text Color
-				wp.customize( 'sidebar_textcolor' ).set( colorScheme[value].colors[4] );
-				sidebarTextColor.val( colorScheme[value].colors[4] )
+				customize( 'sidebar_textcolor' ).set( colorScheme[value].colors[4] );
+				customize.control( 'sidebar_textcolor' ).container.find( '.color-picker-hex' )
 					.data( 'data-default-color', colorScheme[value].colors[4] )
-					.wpColorPicker( 'color', colorScheme[value].colors[4] )
 					.wpColorPicker( 'defaultColor', colorScheme[value].colors[4] );
 			} );
 		}
 	} );
-} )( this.wp );
+} )( this.wp.customize );
