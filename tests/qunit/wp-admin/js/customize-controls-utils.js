f5982b3d05a75189b6816fafc04e1721b4b7a07f
Ryan Kienstra <kienstraryan@gmail.com>
2014-11-03 00:23:36 -0800

    qunit tests for bubbleChildValueChanges

--- tests/qunit/wp-admin/js/customize-controls-utils.js
+++ tests/qunit/wp-admin/js/customize-controls-utils.js
@@ -1,7 +1,8 @@
 /* global wp */
 
 jQuery( function( $ ) {
-	var trueMockEvent, falseMockEvent, mockElementLists, $firstMockElement, $secondMockElement, $thirdMockElement;
+	var trueMockEvent, falseMockEvent, mockElementLists, $firstMockElement, $secondMockElement, $thirdMockElement,
+	    BubbleTester, bubbleTesterParent, firstBubbleTester;
 
 	module( 'Customizer Model Utility functions' );
 
@@ -22,6 +23,7 @@ jQuery( function( $ ) {
 	test( 'isKeydownButNotEnterEvent returns false' , function () {
 		equal( wp.customize.utils.isKeydownButNotEnterEvent( falseMockEvent ) , false );
 	});
+
 
 	$firstMockElement = $( '<div id="foo"></div>' );
 	$secondMockElement = $( '<li id="bar"></li>' );
@@ -49,6 +51,39 @@ jQuery( function( $ ) {
 
 	test( 'areElementListsEqual: lists have same values, but one is longer' , function () {
 		equal( wp.customize.utils.areElementListsEqual( mockElementLists.third , mockElementLists.thirdButLonger ) , false );
+	});
+
+
+	bubbleTesterParent = function() {
+			this.trigger = function( event , instance ) {
+					this.wasChangeTriggered = true;
+					this.instancePassedInTrigger = instance;
+			};
+			this.wasChangeTriggered = false;
+			this.instancePassedInTrigger = {};
+	};
+
+	BubbleTester = wp.customize.Class.extend(
+		{
+			parent : new bubbleTesterParent(),
+			fooValue : new wp.customize.Value(),
+			barValue : new wp.customize.Value(),
+		},
+		{
+			staticProperty : 'propertyValue'
+		}
+	);
+
+	firstBubbleTester = new BubbleTester();
+	wp.customize.utils.bubbleChildValueChanges( firstBubbleTester , [ 'fooValue' ] );
+	firstBubbleTester.fooValue.set( 'new value' );
+
+	test( 'bubbleChildValueChanges notifies parent of change' , function() {
+		ok( firstBubbleTester.parent.wasChangeTriggered );
+	});
+
+	test( 'bubbleChildValueChanges sends its instance to its parent' , function() {
+		ok( firstBubbleTester.parent.instancePassedInTrigger instanceof BubbleTester );
 	});
 
 });
