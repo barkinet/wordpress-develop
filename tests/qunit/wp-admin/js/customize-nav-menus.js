85bb0fce0a2b911527a18058d79806e828914509
Weston Ruter <weston@xwp.co>
2015-07-31 22:46:59 -0400

    Clean up coding style

--- tests/qunit/wp-admin/js/customize-nav-menus.js
+++ tests/qunit/wp-admin/js/customize-nav-menus.js
@@ -2,25 +2,22 @@
 jQuery( function( $ ) {
 
 	var api = wp.customize,
-        settings = window._wpCustomizeNavMenusSettings,
-        navMenu = window.wpNavMenu,
-        customizeSettings = window._wpCustomizeSettings;
-        
+		settings = window._wpCustomizeNavMenusSettings,
+		navMenu = window.wpNavMenu,
+		customizeSettings = window._wpCustomizeSettings;
+
 	module( 'Customize Nav Menus', {
 		setup: function() {
-            window._wpCustomizeNavMenusSettings = settings; 
-            window.wpNavMenu = navMenu;
-            wp.customize.trigger( 'ready' );
+			window._wpCustomizeNavMenusSettings = settings;
+			window.wpNavMenu = navMenu;
+			wp.customize.trigger( 'ready' );
 		},
 		teardown: function() {
-			// restore defaults
-            window._wpCustomizeNavMenusSettings = {}; 
-            window.wpNavMenu = {};
-            window._wpCustomizeSettings = customizeSettings;
-        
+			window._wpCustomizeNavMenusSettings = {};
+			window.wpNavMenu = {};
+			window._wpCustomizeSettings = customizeSettings;
 		}
-	});
-
+	} );
 
 	/**
 	 * Generate 20 ids and verify they are all unique.
@@ -29,23 +26,22 @@ jQuery( function( $ ) {
 		var testIterations = 20,
 			ids = [ api.Menus.generatePlaceholderAutoIncrementId() ];
 
-		while( testIterations-- > 0 ) {
+		while ( testIterations ) {
 			var placeholderID = api.Menus.generatePlaceholderAutoIncrementId();
-
 
 			ok( -1 === ids.indexOf( placeholderID ) );
 			ids.push( placeholderID );
+			testIterations -= 1;
 		}
 
 	} );
 
 	test( 'it should parse _wpCustomizeMenusSettings.defaults into itself', function() {
 		deepEqual( window._wpCustomizeNavMenusSettings, api.Menus.data );
-	});
+	} );
 
-    test( 'empty menus should have no Menu Item Controls', function() {
-        equal( $.isEmptyObject( wp.customize.Menus.getMenuControl( "2" ).getMenuItemControls() ) , true, 'empty menus' );
-    });
+	test( 'empty menus should have no Menu Item Controls', function() {
+		equal( $.isEmptyObject( wp.customize.Menus.getMenuControl( '2' ).getMenuItemControls() ), true, 'empty menus' );
+	} );
 
-
-});
+} );
